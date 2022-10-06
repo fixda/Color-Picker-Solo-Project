6c5053554a7a88e6f4f67cc69f925c12ddbfcb10
fixda <Seanmurphy26@gmail.com>
2022-10-05 21:51:20 -0500

    Cleaning up code

--- index.js
+++ index.js
@@ -10,18 +10,15 @@ function getFetchAddress(){
 function getColorScheme(){
     let colorTest = document.getElementById("color-picker").value
     console.log(document.getElementById("color-scheme").value)
-    console.log(colorTest)
-    getFetchAddress()
-    console.log("apiCall = "+apiCall)
 
+    getFetchAddress()
     fetch(apiCall,{method: "GET"})
         .then(res => res.json())
         .then(data => {
             for(i=0; i<5; i++){
                 colorData[i] = data.colors[i].hex.value 
             }
-            renderScheme()
-    console.log(colorData)})
+            renderScheme()})
 }
 
 function renderScheme(){
